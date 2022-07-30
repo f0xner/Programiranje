47ffb976b01a45f11c929497f7bd8ba2c2151e05
f0xner <krumpak.enei@gmail.com>
2022-07-30 13:18:20 +0200

    day6

--- Advent of Code/2021/day6.py
+++ Advent of Code/2021/day6.py
@@ -1,16 +1,25 @@
-#\!/usr/bin/python3
+#!/usr/bin/python3
 fishes = []
-days = 80
+import numpy as np
+days = 256
 with open("day6IN") as file:
     fishes = file.readline()
     fishes = fishes.split(",")
+    fishes = np.asarray(fishes,dtype=np.uint8)
 for i in range(0,len(fishes)):
     fishes[i] = int(fishes[i])
-for i in range(0,days):
-    for j in range(0,len(fishes)):
-        if fishes[j] == 0:
-            fishes.append(8)
-            fishes[j] = 6
-        else:
-            fishes[j] = fishes[j]-1
-print(len(fishes))
+add = 0
+for x in range(0,days):
+    add = 0
+    i = 0
+    fishes = np.sort(fishes)
+    while(fishes[i]<1):
+        fishes[i] = 6
+        add+=1
+        i+=1
+    while(i<fishes.size):
+        fishes[i]-=1
+        i+=1
+    for j in range(0,add):
+        fishes = np.insert(fishes,fishes.size,8)
+print(fishes.size)
