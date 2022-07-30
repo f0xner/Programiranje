79681992d9804bfd094d920e65aa68a603cd1627
f0xner <krumpak.enei@gmail.com>
2022-07-30 12:14:00 +0200

    day4 second star

--- Advent of Code/2021/day4.py
+++ Advent of Code/2021/day4.py
@@ -1,79 +1,79 @@
 #!/usr/bin/python3
-
-bingos = []
-with open("day4IN") as file:
-    bingos = file.readlines()
-    bingos = [bingo.rstrip() for bingo in bingos]
-
-
-def there_is_winner(board):
-    d2_board = []
-    for i in range(0, 5):
-        d2_board.append([])
-        nums = str(board[i]).split(" ")
-        for j in range(0, 5):
-            if nums[j] != '' and nums[j] != '0-1':
-                d2_board[i].append(int(nums[j]))
-            if nums[j] == '0-1':
-                d2_board[i].append(-1)
-        for i in range(0, 5):
-            r_sum = 0
-            c_sum = 0
-            for j in range(0, 5):
-                try:
-                    r_sum += d2_board[i][j]
-                    c_sum += d2_board[j][i]
-                except:
-                    pass
-            if c_sum == -5 or r_sum == -5:
-                return True
-    # print("d2: ",d2_board)
-    return False
-def unmarked_Sum(board):
-    sum = 0
-    for row in board:
-        nums = row.split(" ")
-        for num in nums:
-            if num != -1 and num != '' and num != '0-1':
-                sum += int(num)
-    return sum
-bingo_numbers = bingos[0].split(",")
-del bingos[0:2]
-bingo_boards = [[]]
-unmarked_sum = 0
-last_drawn = 0
-j = 0
-brk = False
-bingo_winners = []
-for i in range(0, len(bingos)):
-    if bingos[i] != "":
-        bingo_boards[j].append(bingos[i])
-    else:
-        j += 1
-        bingo_boards.append([])
-for bingo_number in bingo_numbers:
-    if int(bingo_number) < 10:
-        bingo_number = "0"+str(bingo_number)
-    for i in range(0,3):
-        for j in range(0, 5):
-            nums = str(bingo_boards[i][j]).split(" ")
-            if bingo_number in nums:
-                bingo_boards[i][j] = bingo_boards[i][j].replace(bingo_number, "0-1")
-        if there_is_winner(bingo_boards[i]):
-            print("w",bingo_boards[i])
-            if i not in bingo_winners:
-                bingo_winners.append(i)
-            if len(bingo_winners) == 2:
-                print("winner")
-                brk = True
-                last_drawn = int(bingo_number)
-                unmarked_sum = unmarked_Sum(bingo_boards[i])
+import numpy as np
+bingo_boards = []
+numbers = []
+np_winner = np.array([-1,-1,-1,-1,-1])
+def setup():
+    global bingo_boards
+    global numbers
+    bingos = []
+    with open("day4IN") as file:
+        bingos = file.readlines()
+        bingos = [bingo.rstrip() for bingo in bingos]
+    numbers = bingos[0].split(",")
+    numbers = np.asarray(numbers,dtype=np.int32)
+    bingos = bingos[1:]
+    i = -1
+    for b in bingos:
+        if b != '':
+            bingo_boards[i].append(b)
+        else:
+            bingo_boards.append([])
+            i+=1
+    for i in range(0,len(bingo_boards)):
+        for j in range(0,len(bingo_boards[i])):
+            bingo_boards[i][j] = bingo_boards[i][j].strip().replace("  "," ").split(" ")
+    bingo_boards = np.asarray(bingo_boards,dtype=np.int32)
+def check_winner(bingo_board):
+    for i in range(0,5):
+        if (bingo_board[i] == np_winner).all():
+           return True
+        j = 0
+        while bingo_board[j][i] == -1:
+            j+=1
+            if j<5:
+                continue
+            else:
                 break
-        if brk:
-            break
-    if brk:
+        if j == 5:
+            return True
+def calc_winner(bingo_board,num):
+    sum = 0
+    for row in bingo_board:
+        sum += np.sum(row,where=row>0)
+    print(bingo_board)
+    print(sum,num)
+    print(sum*num)
+    return True
+def calc_winner_pt2(num,index):
+    global bingo_boards
+    if len(bingo_boards) == 1 and check_winner(bingo_boards[0]):
+        return calc_winner(bingo_boards[0],num)
+    try:
+        if len(bingo_boards) > 1:
+            temp = bingo_boards[:index]
+            bingo_boards = bingo_boards[index+1:]
+            bingo_boards = np.concatenate((temp,bingo_boards))
+            return False
+    except:
+        pass
+def main():
+    global bingo_boards
+    global numbers
+    for num in numbers:
+        for i in range(0,len(bingo_boards)):
+            if num in bingo_boards[i]:
+                res = np.where(bingo_boards[i] == num)
+                bingo_boards[i][res[0][0]][res[1][0]] = -1
+        for i in range(0,len(bingo_boards)):
+            try:
+                if check_winner(bingo_boards[i]):
+                    if calc_winner_pt2(num,i):
+                        break
+            except:
+                pass
+        else:
+            continue
         break
-
-# === part 1 / part 2 ===
-print(unmarked_sum, last_drawn)
-print(unmarked_sum*last_drawn)
+setup()
+main()
